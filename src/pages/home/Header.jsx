a5d5dcf2e274ff358e35163fab94d3e299230c4e
Konson Ak <konakech3@gmail.com>
2023-12-24 12:23:12 +0300

    update header section

--- src/pages/home/Header.jsx
+++ src/pages/home/Header.jsx
@@ -21,20 +21,26 @@ export default function Header() {
 }
 
 function HeaderCard({ slogan }) {
+  const headerText = (cName) => {
+    return (
+      <div className={`flex-1 ${cName} md:px-0 px-8`}>
+        <h2 className="md:text-5xl text-2xl">{slogan.title}</h2>
+        <p className="md:text-2xl text-2xl">{slogan.text1}</p>
+      </div>
+    );
+  };
   return (
     <motion.div
-      className="md:py-20 bg-darkbg text-white px-[10%] flex items-center text-left"
+      className="md:py-20 py-4 bg-darkbg text-white px-[10%] md:flex items-center text-left"
       variants={animateHeaderVariants}
       initial="initial"
       animate="animate"
     >
-      <div className="flex-1 md:block flex flex-col justify-center md:static absolute inset-0 md:px-0 px-8">
-        <h2 className="md:text-5xl text-2xl">{slogan.title}</h2>
-        <p className="md:text-2xl text-2xl">{slogan.text1}</p>
-      </div>
-      <div className="md:h-[60vh] h-[35vh] flex-1">
+      {headerText("md:block hidden")}
+      <div className="md:h-[60vh] h-[30vh] flex-1">
         <img src={slogan.image} alt="" />
       </div>
+      {headerText("md:hidden block")}
     </motion.div>
   );
 }
